0c81586fed61074af2b380a017e7aaae8953a371
Tomás Brancatisano <tomi.brancat@gmail.com>
2023-03-07 19:35:57 -0300

    añadiendo login Juan

--- js/index.js
+++ js/index.js
@@ -1,37 +1,22 @@
-// let account = document.getElementById("login")
 
-// account.addEventListener("click", function () {
-//     
-//     }
-// })
-
-// function confirmedUser() {
-    
-// }
-let loginContent = document.getElementById("login")
-let signInContent = document.getElementById("signIn")
-
-
-function signIn() {
-    loginContent.innerHTML = `<h6 id="signIn"><i class="bi bi-person"></i>Ingresar usuario</h6>`
-    signInContent.addEventListener("click", function () {
+function login() {
+    let user = localStorage.getItem('user')
+    if (user) {
+        alert('Bienvenido ' + user)
+    } else {
         for(let i=3; i>=0; i--){
-            let user = prompt('Ingrese usuario: (Usuario = Juan)')
+            user = prompt('Ingrese usuario: (Usuario = Juan)')
             let pw = parseInt(prompt('Ingrese contraseña: (Contraseña = 12345)')) 
             if(user !== 'Juan' || pw !== 12345){
                 alert('Usuario o contraseña incorrectos, intente denuevo \nIntentos: ' + i)
             }
-            if(user == 'Juan' && pw == 12345) {
-                signed(user)
+            if (user == 'Juan' && pw == 12345) {
+                localStorage.setItem('user', user)
+                alert('Bienvenido ' + user)
                 break
             }
         }
-    })
-}
-function signed(user) {
-    loginContent.innerHTML = ``
+    }
 }
 
-signIn()
-
-
+login()
