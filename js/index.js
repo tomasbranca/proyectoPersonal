e3d3e10808e2014cc145f7350deb973e2e308011
Tomás Brancatisano <tomi.brancat@gmail.com>
2023-03-20 15:40:16 -0300

    arreglando login

--- js/index.js
+++ js/index.js
@@ -1,22 +1,170 @@
-
-function login() {
-    let user = localStorage.getItem('user')
-    if (user) {
-        alert('Bienvenido ' + user)
-    } else {
-        for(let i=3; i>=0; i--){
-            user = prompt('Ingrese usuario: (Usuario = Juan)')
-            let pw = parseInt(prompt('Ingrese contraseña: (Contraseña = 12345)')) 
-            if(user !== 'Juan' || pw !== 12345){
-                alert('Usuario o contraseña incorrectos, intente denuevo \nIntentos: ' + i)
-            }
-            if (user == 'Juan' && pw == 12345) {
-                localStorage.setItem('user', user)
-                alert('Bienvenido ' + user)
-                break
-            }
-        }
-    }
+function saveData() {
+  localStorage.setItem("loginSystem", JSON.stringify(dataBase))
 }
 
-login()
+function loadDataBase() {
+  dataBase = {
+    TomasBranca: {
+      email: "tomi.brancat@gmail.com",
+      password: "beniloli7",
+      role: "admin",
+    }
+  };
+}
+
+async function loginAccount() {
+  let { value: data } = await Swal.fire({
+    title: "Inicio de sesión",
+    html: `<input id="user" class="swal2-input" placeholder="Usuario">
+          <input type="password" id="password" class="swal2-input" placeholder="Contraseña">`,
+    showDenyButton: true,
+    showCancelButton: true,
+    confirmButtonText: "Ingresar",
+    denyButtonText: `Crear usuario`,
+    cancelButtonText: `Cancelar`,
+    focusConfirm: false,
+    preConfirm: () => {
+      user = document.getElementById("user").value;
+      let password = document.getElementById("password").value;
+      if (!user) {
+        Swal.showValidationMessage("Ingrese un usuario");
+        return false;
+      }
+      if (!password) {
+        Swal.showValidationMessage("Ingrese una contraseña");
+        return false;
+      }
+
+      let data = dataBase[user];
+
+      if (!data) {
+        Swal.showValidationMessage("El usuario no existe");
+        return false;
+      }
+
+      if (data.password != password) {
+        Swal.showValidationMessage("La contraseña es incorrecta");
+        return false;
+      }
+
+      return data;
+    },
+  }).then((result) => {
+    if (result.isConfirmed) {
+      accountLoged()
+    } else if (result.isDenied) {
+      createAccount()
+    }
+  });
+  return data;
+}
+
+async function createAccount() {
+  Swal.fire({
+    title: "Crear nueva cuenta",
+    html: `<input type="text" id="user" class="swal2-input" placeholder="Nombre de Usuario">
+          <input type="email" id="email" class="swal2-input" placeholder="E-mail">
+          <input type="password" id="password" class="swal2-input" placeholder="Contraseña">
+          <input type="password" id="confirmPassword" class="swal2-input" placeholder="Confirmar contraseña">`,
+    showCancelButton: true,
+    confirmButtonText: "Crear usuario",
+    cancelButtonText: "Cancelar",
+    focusConfirm: false,
+    preConfirm: () => {
+      user = document.getElementById("user").value;
+      let email = document.getElementById("email").value;
+      let password = document.getElementById("password").value;
+      let confirmPassword = document.getElementById("confirmPassword").value;
+      if (!user) {
+        Swal.showValidationMessage("Ingrese un nombre para la cuenta");
+        return false;
+      }
+      if (!email) {
+        Swal.showValidationMessage("Ingrese un E-Mail para la cuenta");
+        return false;
+      }
+      if (!password) {
+        Swal.showValidationMessage("Ingrese una contraseña para la cuenta");
+        return false;
+      }
+      if (!confirmPassword) {
+        Swal.showValidationMessage("Confirme la contraseña");
+        return false;
+      }
+      if (password != confirmPassword) {
+        Swal.showValidationMessage("Las contraseñas son diferentes");
+        return false;
+      }
+      dataBase[user]={}
+      dataBase[user].email = email
+      dataBase[user].password = password
+      dataBase[user].role = "user"
+      saveData()
+      Toastify({
+        text: "Usuario creado",
+        duration: 3000,
+        gravity: "bottom", 
+        position: "right",
+        backgroundColor: "#E9E9E9",
+        style: {
+          border: "solid 2px #CCD5D9",
+          color: "#071D26",
+        }
+      }).showToast();
+      return true
+      
+    }
+  })
+}
+
+async function accountLoged() {
+  localStorage.setItem('currentUser', user)
+  logDiv.innerHTML = `<div id="account"><h6><i class="bi bi-person-circle"></i> ${user} </h6>
+                      <button type="button" class="btn btn-outline-danger btn-sm" id="logout">Cerrar sesion</button></div>`
+  const Toast = Swal.mixin({
+    toast: true,
+    position: "top-end",
+    showConfirmButton: false,
+    timer: 4000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener("mouseenter", Swal.stopTimer);
+      toast.addEventListener("mouseleave", Swal.resumeTimer);
+    },
+  });
+  Toast.fire({
+    icon: "success",
+    title: "Bienvenido " + user,
+  });
+  let logout = document.getElementById("logout")
+  logout.addEventListener("click", function () {
+    localStorage.removeItem('currentUser')
+    logDiv.innerHTML = `<div id="login"><h6><i class="bi bi-person-fill"></i> Ingresar usuario</h6></div>`
+    let loginDiv = document.getElementById("login")
+    loginDiv.addEventListener("click", loginAccount)
+  })
+}
+
+
+dataBase = JSON.parse(localStorage.getItem("loginSystem"));
+
+let logDiv = document.getElementById("log");
+
+let user = localStorage.getItem('currentUser')
+
+
+if (!dataBase) {
+  loadDataBase();
+}
+
+let accountDiv = document.getElementById("account")
+window.addEventListener("load", function () {
+  if (!user) {
+    logDiv.innerHTML = `<div id="login"><h6><i class="bi bi-person-fill"></i> Ingresar usuario</h6></div>`
+    let loginDiv = document.getElementById("login")
+    loginDiv.addEventListener("click", loginAccount)
+  }
+  else if (user) {
+    accountLoged()
+  }
+})
--- js/stats.js
+++ js/stats.js
@@ -1,228 +1,229 @@
 function tableGenerator() {
-    for (let i = 0; i < statsPlayer.length; i++){
-        tableContent +=
-        `<tr>
+  for (let i = 0; i < statsPlayer.length; i++) {
+    tableContent += `<tr>
             <th scope = "row"> ${statsPlayer[i][0]} </th>
-            <td class="centrado"> ${(statsPlayer[i][1])} </td>
-            <td class="centrado"> ${(statsPlayer[i][2])} </td>
-            <td class="centrado"> ${(statsPlayer[i][3])} </td>
-        </tr>`
-    }
-    statsPlayerTable.innerHTML = tableContent 
+            <td class="centrado"> ${statsPlayer[i][1]} </td>
+            <td class="centrado"> ${statsPlayer[i][2]} </td>
+            <td class="centrado"> ${statsPlayer[i][3]} </td>
+        </tr>`;
+  }
+  statsPlayerTable.innerHTML = tableContent;
 }
 function ammount() {
-    for (let i1 = 0; i1 < games.length; i1++){
-        for (let i2 = 0; i2 < games[i1].length; i2++){
-            statsPlayer[i2][1] += games[i1][i2].points
-            statsPlayer[i2][2] += games[i1][i2].rebounds
-            statsPlayer[i2][3] += games[i1][i2].assists
-        }
+  for (let i1 = 0; i1 < games.length; i1++) {
+    for (let i2 = 0; i2 < games[i1].length; i2++) {
+      statsPlayer[i2][1] += games[i1][i2].points;
+      statsPlayer[i2][2] += games[i1][i2].rebounds;
+      statsPlayer[i2][3] += games[i1][i2].assists;
     }
-    average()
+  }
+  average();
 }
 
 function average() {
-    for (let i = 0; i < statsPlayer.length; i++){
-        statsPlayer[i][1] = statsPlayer[i][1] / games.length
-        statsPlayer[i][2] = statsPlayer[i][2] / games.length
-        statsPlayer[i][3] = statsPlayer[i][3] / games.length
-    }
-    fixed()
+  for (let i = 0; i < statsPlayer.length; i++) {
+    statsPlayer[i][1] = statsPlayer[i][1] / games.length;
+    statsPlayer[i][2] = statsPlayer[i][2] / games.length;
+    statsPlayer[i][3] = statsPlayer[i][3] / games.length;
+  }
+  fixed();
 }
 
 function fixed() {
-    for (let i = 0; i < statsPlayer.length; i++){
-        statsPlayer[i][1] = statsPlayer[i][1].toFixed(1).padStart(4, "0")
-        statsPlayer[i][2] = statsPlayer[i][2].toFixed(1).padStart(4, "0")
-        statsPlayer[i][3] = statsPlayer[i][3].toFixed(1).padStart(4, "0")
-    }
+  for (let i = 0; i < statsPlayer.length; i++) {
+    statsPlayer[i][1] = statsPlayer[i][1].toFixed(1).padStart(4, "0");
+    statsPlayer[i][2] = statsPlayer[i][2].toFixed(1).padStart(4, "0");
+    statsPlayer[i][3] = statsPlayer[i][3].toFixed(1).padStart(4, "0");
+  }
 }
-    
+
 function statSort() {
-    for (let i = 0; i < thStats.length; i++){
-        thStats[i].addEventListener("click", statClicked)
-    }
+  for (let i = 0; i < thStats.length; i++) {
+    thStats[i].addEventListener("click", statClicked);
+  }
 }
 
 function statClicked(e) {
-    const sortColumn = e.target.cellIndex !== undefined ? e.target.cellIndex : e.target.parentNode.cellIndex
-    const icons = document.querySelectorAll(".icon")
-    icons.forEach(icon => {
-        if (icon.classList.contains("icon-active")) {
-            icon.classList.remove("icon-active")
-        }
-        if (icon.parentNode.cellIndex === sortColumn) {
-            icon.classList.add("icon-active")
-        }
-    })
-    sortTableByStat(sortColumn)
+  const sortColumn =
+    e.target.cellIndex !== undefined
+      ? e.target.cellIndex
+      : e.target.parentNode.cellIndex;
+  const icons = document.querySelectorAll(".icon");
+  icons.forEach((icon) => {
+    if (icon.classList.contains("icon-active")) {
+      icon.classList.remove("icon-active");
+    }
+    if (icon.parentNode.cellIndex === sortColumn) {
+      icon.classList.add("icon-active");
+    }
+  });
+  sortTableByStat(sortColumn);
 }
 
 function sortTableByStat(sortColumn) {
-    const tableBody = document.getElementById("statsPlayer")
-    const rows = Array.from(tableBody.rows)
-    let ascClass = tableBody.classList.contains("asc")
-    let asc = !ascClass ? true : false
-    let sortedRows
-    if (asc === false) {
-        sortedRows = rows.sort(function (a, b) {
-            const aText = a.cells[sortColumn].textContent
-            const bText = b.cells[sortColumn].textContent
-            return aText.localeCompare(bText)
-        })
-    }
-    if (asc === true) {
-        sortedRows = rows.sort(function (a, b) {
-            const aText = a.cells[sortColumn].textContent
-            const bText = b.cells[sortColumn].textContent
-            return bText.localeCompare(aText)
-        })
-        
-    }
-    tableBody.innerHTML = ""
-    sortedRows.forEach(row => {
-        tableBody.appendChild(row)
-    })
-    if (asc === true) {
-        tableBody.classList.add("asc")
-    } else {
-        tableBody.classList.remove("asc")
-    }
+  const tableBody = document.getElementById("statsPlayer");
+  const rows = Array.from(tableBody.rows);
+  let ascClass = tableBody.classList.contains("asc");
+  let asc = !ascClass ? true : false;
+  let sortedRows;
+  if (asc === false) {
+    sortedRows = rows.sort(function (a, b) {
+      const aText = a.cells[sortColumn].textContent;
+      const bText = b.cells[sortColumn].textContent;
+      return aText.localeCompare(bText);
+    });
+  }
+  if (asc === true) {
+    sortedRows = rows.sort(function (a, b) {
+      const aText = a.cells[sortColumn].textContent;
+      const bText = b.cells[sortColumn].textContent;
+      return bText.localeCompare(aText);
+    });
+  }
+  tableBody.innerHTML = "";
+  sortedRows.forEach((row) => {
+    tableBody.appendChild(row);
+  });
+  if (asc === true) {
+    tableBody.classList.add("asc");
+  } else {
+    tableBody.classList.remove("asc");
+  }
 }
 
-window.addEventListener("load", function() {
-    document.getElementById("points").click()
-})
+window.addEventListener("load", function () {
+  document.getElementById("points").click();
+});
 
-let tableContent = ""
+let tableContent = "";
 
-let thStats = document.getElementsByClassName("stats-box")
+let thStats = document.getElementsByClassName("stats-box");
 
-const statsPlayerTable = document.getElementById('statsPlayer')
+const statsPlayerTable = document.getElementById("statsPlayer");
 
 let statsPlayer = [
-    ["Davis Bertans", 0, 0, 0],
-    ["Reggie Bullock", 0, 0, 0],
-    ["Luka Doncic", 0, 0, 0],
-    ["Josh Green", 0, 0, 0],
-    ["Tim Hardaway Jr.", 0, 0, 0],
-    ["Jaden Hardy", 0, 0, 0],
-    ["Justin Holiday", 0, 0, 0],
-    ["Kyrie Irving", 0, 0, 0],
-    ["Maxi Kleber", 0, 0, 0],
-    ["Dwight Powell", 0, 0, 0],
-    ["Christian Wood", 0, 0, 0]
-]
+  ["Davis Bertans", 0, 0, 0],
+  ["Reggie Bullock", 0, 0, 0],
+  ["Luka Doncic", 0, 0, 0],
+  ["Josh Green", 0, 0, 0],
+  ["Tim Hardaway Jr.", 0, 0, 0],
+  ["Jaden Hardy", 0, 0, 0],
+  ["Justin Holiday", 0, 0, 0],
+  ["Kyrie Irving", 0, 0, 0],
+  ["Maxi Kleber", 0, 0, 0],
+  ["Dwight Powell", 0, 0, 0],
+  ["Christian Wood", 0, 0, 0],
+];
 
 let games = [
-    [
-        { name: 'Bertans', points: 4, rebounds: 1, assists: 0 },
-        { name: 'Bullock', points: 7, rebounds: 2, assists: 1 },
-        { name: 'Doncic', points: 33, rebounds: 10, assists: 7 },
-        { name: 'Green', points: 10, rebounds: 2, assists: 2 },
-        { name: 'Hardaway', points: 8, rebounds: 0, assists: 0 },
-        { name: 'Hardy', points: 2, rebounds: 0, assists: 1 },
-        { name: 'Holiday', points: 6, rebounds: 2, assists: 0 },
-        { name: 'Irving', points: 25, rebounds: 4, assists: 7 },
-        { name: 'Kleber', points: 8, rebounds: 4, assists: 1 },
-        { name: 'Powell', points: 7, rebounds: 7, assists: 0 },
-        { name: 'Wood', points: 16, rebounds: 8, assists: 2 },
-    ],
-    [
-        { name: 'Bertans', points: 1, rebounds: 2, assists: 0 },
-        { name: 'Bullock', points: 12, rebounds: 5, assists: 0 },
-        { name: 'Doncic', points: 28, rebounds: 4, assists: 12 },
-        { name: 'Green', points: 9, rebounds: 1, assists: 3 },
-        { name: 'Hardaway', points: 10, rebounds: 1, assists: 1 },
-        { name: 'Hardy', points: 6, rebounds: 1, assists: 0 },
-        { name: 'Holiday', points: 3, rebounds: 4, assists: 2 },
-        { name: 'Irving', points: 28, rebounds: 5, assists: 4 },
-        { name: 'Kleber', points: 6, rebounds: 6, assists: 1 },
-        { name: 'Powell', points: 4, rebounds: 6, assists: 0 },
-        { name: 'Wood', points: 12, rebounds: 5, assists: 0 },
-    ],
-    [
-        { name: 'Bertans', points: 0, rebounds: 2, assists: 1 },
-        { name: 'Bullock', points: 13, rebounds: 4, assists: 2 },
-        { name: 'Doncic', points: 38, rebounds: 12, assists: 5 },
-        { name: 'Green', points: 5, rebounds: 0, assists: 1 },
-        { name: 'Hardaway', points: 16, rebounds: 2, assists: 3 },
-        { name: 'Hardy', points: 3, rebounds: 1, assists: 0 },
-        { name: 'Holiday', points: 7, rebounds: 0, assists: 1 },
-        { name: 'Irving', points: 22, rebounds: 5, assists: 3 },
-        { name: 'Kleber', points: 4, rebounds: 8, assists: 0 },
-        { name: 'Powell', points: 3, rebounds: 10, assists: 1 },
-        { name: 'Wood', points: 12, rebounds: 7, assists: 1 },
-    ],
-    [
-        { name: 'Bertans', points: 6, rebounds: 3, assists: 1 },
-        { name: 'Bullock', points: 3, rebounds: 4, assists: 0 },
-        { name: 'Doncic', points: 34, rebounds: 14, assists: 12 },
-        { name: 'Green', points: 8, rebounds: 1, assists: 3 },
-        { name: 'Hardaway', points: 8, rebounds: 3, assists: 2 },
-        { name: 'Hardy', points: 4, rebounds: 2, assists: 0 },
-        { name: 'Holiday', points: 9, rebounds: 1, assists: 1 },
-        { name: 'Irving', points: 26, rebounds: 7, assists: 4 },
-        { name: 'Kleber', points: 5, rebounds: 6, assists: 0 },
-        { name: 'Powell', points: 8, rebounds: 4, assists: 0 },
-        { name: 'Wood', points: 22, rebounds: 10, assists: 1 },
-    ],
-    [
-        { name: 'Bertans', points: 2, rebounds: 0, assists: 0 },
-        { name: 'Bullock', points: 2, rebounds: 4, assists: 1 },
-        { name: 'Doncic', points: 41, rebounds: 10, assists: 15 },
-        { name: 'Green', points: 6, rebounds: 3, assists: 1 },
-        { name: 'Hardaway', points: 12, rebounds: 2, assists: 2 },
-        { name: 'Hardy', points: 0, rebounds: 0, assists: 0 },
-        { name: 'Holiday', points: 5, rebounds: 1, assists: 0 },
-        { name: 'Irving', points: 33, rebounds: 6, assists: 6 },
-        { name: 'Kleber', points: 4, rebounds: 4, assists: 0 },
-        { name: 'Powell', points: 5, rebounds: 7, assists: 1 },
-        { name: 'Wood', points: 20, rebounds: 6, assists: 1 },
-    ],
-    [
-        { name: 'Bertans', points: 8, rebounds: 3, assists: 2 },
-        { name: 'Bullock', points: 12, rebounds: 1, assists: 2 },
-        { name: 'Doncic', points: 24, rebounds: 16, assists: 12 },
-        { name: 'Green', points: 12, rebounds: 3, assists: 1 },
-        { name: 'Hardaway', points: 5, rebounds: 0, assists: 1 },
-        { name: 'Hardy', points: 6, rebounds: 1, assists: 0 },
-        { name: 'Holiday', points: 8, rebounds: 4, assists: 3 },
-        { name: 'Irving', points: 30, rebounds: 3, assists: 6 },
-        { name: 'Kleber', points: 10, rebounds: 5, assists: 0 },
-        { name: 'Powell', points: 5, rebounds: 4, assists: 2 },
-        { name: 'Wood', points: 15, rebounds: 10, assists: 1 },
-    ],
-    [
-        { name: 'Bertans', points: 0, rebounds: 0, assists: 0 },
-        { name: 'Bullock', points: 4, rebounds: 2, assists: 1 },
-        { name: 'Doncic', points: 30, rebounds: 10, assists: 5 },
-        { name: 'Green', points: 9, rebounds: 0, assists: 3 },
-        { name: 'Hardaway', points: 13, rebounds: 2, assists: 0 },
-        { name: 'Hardy', points: 4, rebounds: 0, assists: 2 },
-        { name: 'Holiday', points: 4, rebounds: 2, assists: 1 },
-        { name: 'Irving', points: 24, rebounds: 3, assists: 5 },
-        { name: 'Kleber', points: 8, rebounds: 3, assists: 2 },
-        { name: 'Powell', points: 7, rebounds: 7, assists: 0 },
-        { name: 'Wood', points: 20, rebounds: 12, assists: 1 },
-    ],
-    [
-        { name: 'Bertans', points: 2, rebounds: 0, assists: 0 },
-        { name: 'Bullock', points: 6, rebounds: 2, assists: 1 },
-        { name: 'Doncic', points: 33, rebounds: 8, assists: 6 },
-        { name: 'Green', points: 12, rebounds: 1, assists: 4 },
-        { name: 'Hardaway', points: 17, rebounds: 0, assists: 2 },
-        { name: 'Hardy', points: 5, rebounds: 1, assists: 1 },
-        { name: 'Holiday', points: 2, rebounds: 2, assists: 4 },
-        { name: 'Irving', points: 22, rebounds: 4, assists: 8 },
-        { name: 'Kleber', points: 8, rebounds: 5, assists: 2 },
-        { name: 'Powell', points: 7, rebounds: 10, assists: 0 },
-        { name: 'Wood', points: 17, rebounds: 5, assists: 1 },
-    ],
-]
+  [
+    { name: "Bertans", points: 4, rebounds: 1, assists: 0 },
+    { name: "Bullock", points: 7, rebounds: 2, assists: 2 },
+    { name: "Doncic", points: 33, rebounds: 7, assists: 7 },
+    { name: "Green", points: 10, rebounds: 2, assists: 2 },
+    { name: "Hardaway", points: 11, rebounds: 2, assists: 1 },
+    { name: "Hardy", points: 2, rebounds: 0, assists: 1 },
+    { name: "Holiday", points: 6, rebounds: 2, assists: 0 },
+    { name: "Irving", points: 25, rebounds: 4, assists: 7 },
+    { name: "Kleber", points: 8, rebounds: 4, assists: 1 },
+    { name: "Powell", points: 7, rebounds: 5, assists: 0 },
+    { name: "Wood", points: 16, rebounds: 8, assists: 2 },
+  ],
+  [
+    { name: "Bertans", points: 1, rebounds: 2, assists: 1 },
+    { name: "Bullock", points: 12, rebounds: 5, assists: 0 },
+    { name: "Doncic", points: 28, rebounds: 4, assists: 12 },
+    { name: "Green", points: 9, rebounds: 1, assists: 3 },
+    { name: "Hardaway", points: 27, rebounds: 5, assists: 1 },
+    { name: "Hardy", points: 6, rebounds: 1, assists: 3 },
+    { name: "Holiday", points: 3, rebounds: 4, assists: 2 },
+    { name: "Irving", points: 28, rebounds: 5, assists: 6 },
+    { name: "Kleber", points: 6, rebounds: 2, assists: 1 },
+    { name: "Powell", points: 4, rebounds: 4, assists: 0 },
+    { name: "Wood", points: 12, rebounds: 5, assists: 2 },
+  ],
+  [
+    { name: "Bertans", points: 0, rebounds: 2, assists: 1 },
+    { name: "Bullock", points: 13, rebounds: 4, assists: 3 },
+    { name: "Doncic", points: 38, rebounds: 10, assists: 5 },
+    { name: "Green", points: 5, rebounds: 3, assists: 1 },
+    { name: "Hardaway", points: 16, rebounds: 2, assists: 3 },
+    { name: "Hardy", points: 7, rebounds: 3, assists: 2 },
+    { name: "Holiday", points: 7, rebounds: 0, assists: 1 },
+    { name: "Irving", points: 22, rebounds: 5, assists: 7 },
+    { name: "Kleber", points: 4, rebounds: 4, assists: 2 },
+    { name: "Powell", points: 3, rebounds: 7, assists: 1 },
+    { name: "Wood", points: 12, rebounds: 7, assists: 1 },
+  ],
+  [
+    { name: "Bertans", points: 6, rebounds: 3, assists: 1 },
+    { name: "Bullock", points: 3, rebounds: 4, assists: 0 },
+    { name: "Doncic", points: 34, rebounds: 11, assists: 11 },
+    { name: "Green", points: 8, rebounds: 4, assists: 3 },
+    { name: "Hardaway", points: 8, rebounds: 3, assists: 2 },
+    { name: "Hardy", points: 15, rebounds: 2, assists: 0 },
+    { name: "Holiday", points: 6, rebounds: 1, assists: 1 },
+    { name: "Irving", points: 26, rebounds: 7, assists: 6 },
+    { name: "Kleber", points: 5, rebounds: 5, assists: 0 },
+    { name: "Powell", points: 8, rebounds: 4, assists: 0 },
+    { name: "Wood", points: 22, rebounds: 10, assists: 1 },
+  ],
+  [
+    { name: "Bertans", points: 6, rebounds: 0, assists: 0 },
+    { name: "Bullock", points: 2, rebounds: 4, assists: 1 },
+    { name: "Doncic", points: 41, rebounds: 10, assists: 8 },
+    { name: "Green", points: 6, rebounds: 3, assists: 1 },
+    { name: "Hardaway", points: 12, rebounds: 4, assists: 2 },
+    { name: "Hardy", points: 10, rebounds: 2, assists: 0 },
+    { name: "Holiday", points: 5, rebounds: 1, assists: 0 },
+    { name: "Irving", points: 33, rebounds: 6, assists: 6 },
+    { name: "Kleber", points: 4, rebounds: 3, assists: 0 },
+    { name: "Powell", points: 5, rebounds: 3, assists: 1 },
+    { name: "Wood", points: 20, rebounds: 6, assists: 1 },
+  ],
+  [
+    { name: "Bertans", points: 8, rebounds: 3, assists: 2 },
+    { name: "Bullock", points: 12, rebounds: 1, assists: 2 },
+    { name: "Doncic", points: 24, rebounds: 12, assists: 10 },
+    { name: "Green", points: 12, rebounds: 5, assists: 1 },
+    { name: "Hardaway", points: 10, rebounds: 3, assists: 1 },
+    { name: "Hardy", points: 5, rebounds: 3, assists: 0 },
+    { name: "Holiday", points: 4, rebounds: 4, assists: 3 },
+    { name: "Irving", points: 30, rebounds: 5, assists: 6 },
+    { name: "Kleber", points: 7, rebounds: 5, assists: 0 },
+    { name: "Powell", points: 7, rebounds: 4, assists: 2 },
+    { name: "Wood", points: 15, rebounds: 10, assists: 1 },
+  ],
+  [
+    { name: "Bertans", points: 3, rebounds: 0, assists: 0 },
+    { name: "Bullock", points: 4, rebounds: 2, assists: 1 },
+    { name: "Doncic", points: 30, rebounds: 7, assists: 5 },
+    { name: "Green", points: 15, rebounds: 2, assists: 3 },
+    { name: "Hardaway", points: 13, rebounds: 4, assists: 0 },
+    { name: "Hardy", points: 7, rebounds: 2, assists: 2 },
+    { name: "Holiday", points: 4, rebounds: 2, assists: 1 },
+    { name: "Irving", points: 24, rebounds: 4, assists: 5 },
+    { name: "Kleber", points: 5, rebounds: 4, assists: 2 },
+    { name: "Powell", points: 8, rebounds: 5, assists: 0 },
+    { name: "Wood", points: 20, rebounds: 12, assists: 1 },
+  ],
+  [
+    { name: "Bertans", points: 5, rebounds: 0, assists: 0 },
+    { name: "Bullock", points: 6, rebounds: 2, assists: 1 },
+    { name: "Doncic", points: 33, rebounds: 5, assists: 6 },
+    { name: "Green", points: 12, rebounds: 3, assists: 4 },
+    { name: "Hardaway", points: 17, rebounds: 4, assists: 2 },
+    { name: "Hardy", points: 8, rebounds: 1, assists: 1 },
+    { name: "Holiday", points: 2, rebounds: 2, assists: 4 },
+    { name: "Irving", points: 22, rebounds: 5, assists: 8 },
+    { name: "Kleber", points: 8, rebounds: 3, assists: 2 },
+    { name: "Powell", points: 10, rebounds: 4, assists: 0 },
+    { name: "Wood", points: 17, rebounds: 5, assists: 1 },
+  ],
+];
 
-ammount ()
+ammount();
 
-tableGenerator()
+tableGenerator();
 
-statSort()
+statSort();
